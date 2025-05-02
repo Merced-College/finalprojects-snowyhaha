66250f52d4c4354bc1eac61455dc1d18bdd0e427
snowyhaha <Catty23435@gmail.com>
2025-05-01 20:03:44 -0700

    Add files via upload
    This is what I have started. I am creating an application called StudyBuddy that will allow you to study flashcards and be able to quiz yourself while figuring out what you have missed. It will also keep track of scores and what you have gotten wrong. The code I have written so far is the Flashcard class where it creates each card and returns it. I also have created a deck of cards which you will be able to add cards to. I plan to finish the quiz class and be able to run it through the main. I will also figure out how to make things more interesting.

---
+++ Final.java
@@ -0,0 +1,13 @@
+import java.util.Hashtable;
+import java.util.Scanner;
+import java.util.LinkedList;
+
+public class Final {
+
+	public static void main(String[] args) {
+		 LinkedList<String> FlashCard = new LinkedList<>();
+		 Hashtable<String, FlashCard> subjects = new Hashtable<>();
+		 System.out.print("testing");
+	}
+
+}
